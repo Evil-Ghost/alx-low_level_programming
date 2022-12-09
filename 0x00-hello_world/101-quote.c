e4b26648c0d979f45fc4070493091fa5270f8cf6
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-09 12:49:51 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+
+/**
+  * main - Prints string
+  * Return: this program will return a value of 1
+  */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+	return (1);
+}
