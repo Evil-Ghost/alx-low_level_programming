014f3e0a45d6558c4ccc4b92894bab34110232e3
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-08 21:37:16 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+  * main - Prints string followed by a new line
+  *Return: it returns the value 0 thereby ending the program
+  */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
