2258a0d0f343db32d5f6ab36c6c881bb34d384b1
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-14 14:40:55 -0800

    Correct exercise

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,7 +10,15 @@ int print_last_digit(int l)
 {
 	int last_digit;
 
-	last_digit = l % 10;
+	if (l < 0)
+	{
+		last_digit = (l * -1) % 10;
+	}
+	else
+	{
+
+		last_digit = l % 10;
+	}
 	_putchar(last_digit + '0');
 
 	return (last_digit);
