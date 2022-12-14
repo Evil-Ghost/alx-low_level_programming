91b52f85ca755f80f88183a15c8927ead188a37b
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-13 21:48:38 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints lowercase alphabets 10 times
+ * one for each line
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	i = 0;
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
