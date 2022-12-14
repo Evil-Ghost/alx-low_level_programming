fc661e8421d0511980b5789f6ca33ddd91a6dd85
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-14 12:36:56 -0800

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n: number to be checked
+ *
+ * Return: 1 if n is greater than '0', 0 if n
+ * is '0' and -1 if n is less than '0'
+ */
+int print_sign(int c)
+{
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
