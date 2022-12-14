72b6ce23f0f2f705d8fd9cd7b4fee83ed0886b71
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-13 21:29:46 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints lowercase alphabets and a new line
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	c = 'a';
+	while (c <= 'z')
+	{
+		_putchar('c');
+	}
+}
+
+/**
+ * main - call the print_alphabet function
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
