d40dd7168b1a0ef8415ecf9f72997e3ed52b944e
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-15 06:22:25 -0800

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute of the day
+ * starting from 00:00 to 23:59
+ */
+void jack_bauer(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	i = 0;
+	while (i <= 2)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (i == 2 && j < 4)
+			{
+				break;
+			}
+			k = 0;
+			while (k < 6)
+			{
+				l = 0;
+				while (l < 10)
+				{
+					_putchar(i % 10 + '0');
+					_putchar(j % 10 + '0');
+					_putchar(':');
+					_putchar(k % 10 + '0');
+					_putchar(l % 10 + '0');
+					_putchar('\n');
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
