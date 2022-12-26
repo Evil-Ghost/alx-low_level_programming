fbb5f2a31a3a71902d5b7f5ae26fab3e72b9ec7c
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-26 04:32:32 -0800

    Software is eating the world

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - prints all combination of two-digit
+ * numbers such that the same combinations are
+ * not printed e.g 00 10 and 01 00
+ *
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while(j < 10)
+		{
+			k = i;
+			while (k < 10)
+			{
+				l = j + 1;
+				while (l < 10)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+
+					if (!(i == 9 && j == 8 && k == 9 && l == 9))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					l++;
+				}
+				k++;
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
