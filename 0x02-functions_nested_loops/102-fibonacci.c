70143085a3515c7207abce5f69a7499d6e534393
Evil-Ghost <danielaisosajoseph@outlook.com>
2022-12-26 20:16:48 -0800

    50 fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first fibonacci numbers
+ * 
+ * Return: Always 0(success)
+ */
+int main(void)
+{
+	long i, a, b, sum;
+
+	i = 1;
+	a = 0;
+	b = 1;
+	sum = 0;
+	while (i <= 50)
+	{
+		sum = a + b;
+		a = b;
+		b = sum;
+		printf("%ld", sum);
+		if (i != 50)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
