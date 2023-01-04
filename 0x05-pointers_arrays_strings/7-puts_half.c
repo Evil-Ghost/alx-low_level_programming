a44ca0d26325a68d787f4553cd5d3912728cd5f8
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-03 16:46:19 -0800

    Task 7: half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string called by function
+ *
+ * Return: no return value(void)
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+
+	i = 0;
+	while (*(str + i) != 0)
+	{
+		i++;
+	}
+
+	if (i % 2 != 0)
+		n = (i - 1) / 2;
+	else
+		n = i / 2;
+
+	while (n <= i)
+	{
+		_putchar(*(str + n));
+		n++;
+	}
+	_putchar('\n');
+}
