e3b55bc512f0728d578900602be06631d4f82110
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-03 18:01:44 -0800

    Task 5: reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string and change the stored value
+ * @s: string passed into function
+ *
+ * Return: no return value(void)
+ */
+void rev_string(char *s)
+{
+	int i, j, k;
+	char a[99];
+	
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		*(a + i) = *(s + i);
+		i++;
+	}
+	*(a + i) = '\0';
+	j = 0;
+	i -= 1;
+	k = i;
+	while (j <= k)
+	{
+		*(s + j) = *(a + i);
+		i--;
+		j++;
+	}
+}
