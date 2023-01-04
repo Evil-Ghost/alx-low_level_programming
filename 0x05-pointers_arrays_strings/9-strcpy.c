67ddfe4b81e234c58101e4a9f081fa828db471f3
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-04 04:29:48 -0800

    Task 9: copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy the string pointed to by src to dest
+ * @dest: destination of string
+ * @src: string to be copied
+ * 
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (1)
+	{
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+		{
+			break;
+		}
+		i++;
+	}
+	return dest;
+}
