abd4780127cf60af4d7dfafe04a229a4210987fc
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-03 16:28:41 -0800

    Task 6: each two memory

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every character of a string skipping one each
+ * @str: pointer to hold variable address
+ *
+ * Return: no return value(void)
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != 0)
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+	_putchar('\n');
+}
