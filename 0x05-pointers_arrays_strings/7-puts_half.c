266547d41506fd6cd35055082c89d48ac89bb5c5
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-04 03:17:18 -0800

    Make correction

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -16,13 +16,20 @@ void puts_half(char *str)
 	{
 		i++;
 	}
+	_putchar((i / 2) % 10 + '0');
+	_putchar(i % 10 + '0');
+	_putchar('\n');
 
 	if (i % 2 != 0)
+	{
 		n = (i - 1) / 2;
+		n += 1;
+	}
 	else
+	{
 		n = i / 2;
+	}
 
-	n += 1;
 	while (n < i)
 	{
 		_putchar(*(str + n));
