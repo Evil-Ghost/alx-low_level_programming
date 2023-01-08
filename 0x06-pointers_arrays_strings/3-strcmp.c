30fecb001e5d7536c5cadb35b2641a20ef39d6fc
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-08 15:01:01 -0800

    Task 3: compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strcmp - compare two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns value of s1 - s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+	return (s1[0] - s2[0]);
+}
