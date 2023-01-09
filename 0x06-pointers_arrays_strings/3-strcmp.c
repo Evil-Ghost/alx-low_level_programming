0800cf7595877c5b374f69e07a0724c7d46fb153
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-08 16:09:52 -0800

    Correct solution

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,5 +9,21 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	return (*(s1 + 5) - *(s2 + 5));
+	int i;
+
+	i = 0;
+	while (1)
+	{
+		if (*(s1 + i) - *(s2 + i) != 0)
+		{
+			return (*(s1 + i) - *(s2 + i));
+			break;
+		}
+		else if (*(s1 + i) == '0')
+		{
+			return (0);
+			break;
+		}
+		i++;
+	}
 }
