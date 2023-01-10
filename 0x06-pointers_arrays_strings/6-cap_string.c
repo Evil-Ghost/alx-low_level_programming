825fd8ae8c647a1131a16d9a6be53c517f8c3284
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-10 13:26:49 -0800

    Task 6: capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes the first letter of a word
+ * @s: string to be capitalized
+ *
+ * Return: returns string to ptr
+ */
+char *cap_string(char *s)
+{
+	int i;
+	int j;
+	int rev[14] = {32, 9, 11, 10, 44, 46, 59, 33, 63, 34, 40, 41, 123, 125};
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (j < 14)
+		{
+			if (*(s + i) - rev[j] == 0)
+			{
+				if (*(s + (i + 1)) >= 97 && *(s + (i + 1)) <= 122)
+				{
+					*(s + (i + 1)) = (*(s + (i + 1)) - 97) + 65;
+				}
+			}
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
