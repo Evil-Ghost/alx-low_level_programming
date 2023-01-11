82da30296746d4482264de1e34c77f6cd71c5a8f
Evil-Ghost <danielaisosajoseph@outlook.com>
2023-01-10 16:28:05 -0800

    Task 7: encodes a string to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @s: string passed into function
+ *
+ * Return: returns new string to pointer
+ */
+char *leet(char *s)
+{
+	int i;
+	int j;
+	int let[10] = {65, 97, 69, 102, 79, 111, 84, 116, 76, 108};
+	int num[10] = {52, 52, 51, 51, 48, 48, 55, 55, 49, 49};
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (*(s + i) - let[j] == 0)
+				*(s + i) = num[j];
+			j++;
+		}
+		i++;
+	}
+	return (s);
+}
