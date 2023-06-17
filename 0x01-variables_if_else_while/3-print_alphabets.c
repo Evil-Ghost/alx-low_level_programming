608215f75133ae8f4f79a66baf02565c25a30e52
evil-ghost <danielaisosajoseph@outlook.com>
2023-06-16 21:19:44 -0700

    Task 3: complete task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet on the console in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 97; n <= 122; n++)
+		putchar(n);
+
+	for (n = 65; n <= 90; n++)
+		putchar(n);
+
+	putchar('\n');
+
+	return (0);
+}
