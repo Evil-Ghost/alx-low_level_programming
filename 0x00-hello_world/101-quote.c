e0b52026e27ade648a67ad41cf2aab7f32b66f46
evil-ghost <danielaisosajoseph@outlook.com>
2023-06-18 20:14:54 -0700

    Task 8: complete task

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+
+/**
+ * main - prints a string without using the stdio library
+ *
+ * Return: Alwasy (0) success
+ */
+int main(void)
+{
+	system("echo 'and that piece of art is useful\" - Dora Korpar, 2015-10-19'");
+
+	return (0);
+}
