c398868bd791682d5bc74076fd1283671f6bfd12
evil-ghost <danielaisosajoseph@outlook.com>
2023-06-22 11:40:36 -0700

    Task 10: complete task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers
+ * @a: first integer argument
+ * @b: second integer argument
+ *
+ * Return: returns the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
