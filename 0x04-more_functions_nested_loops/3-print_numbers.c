52ba83e7659aac1e5960ab1f3a9b5900b4d6abfb
evil-ghost <danielaisosajoseph@outlook.com>
2023-06-22 21:59:08 -0700

    Task 3: complete task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.c"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		_putchar(i);
+		i++;
+	}
+}
