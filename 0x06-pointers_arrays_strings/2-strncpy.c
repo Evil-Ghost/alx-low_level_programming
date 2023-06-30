d1004b9689da2b4fcaa7fe0b4710944a07f98f5f
evil-ghost <danielaisosajoseph@outlook.com>
2023-06-29 19:53:49 -0700

    Task 2: fix code to correct output

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,18 +10,18 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i, j;
+	int i;
 
 
 	i = 0;
-	j = 0;
-	while (j < n && *(src + j) != '\0')
+	while (i < n && *(src + i) != '\0')
 	{
-		*(dest + i) = *(src + j);
+		*(dest + i) = *(src + i);
+		if (*(src + i) == '\0')
+			break;
 		i++;
-		j++;
 	}
-	if (*(src + j) == '\0')
+	for (;i < n; i++)
 		*(dest + i) = '\0';
 
 	return (dest);
