50ce6ad806142cb8d49e332809b99f7b7c803896
evil-ghost <danielaisosajoseph@outlook.com>
2023-07-03 11:13:46 -0700

    Task 1: fix betty warning

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memcpy() - copies memory area
+ * _memcpy - copies memory area
  * @dest: destination of the copied memory area
  * @src: memory area to be copied
  * @n: number of bytes to be copied
