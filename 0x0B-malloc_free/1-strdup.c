c417d1c53d4bd68654e01b10716cf99ce252ba35
evil-ghost <danielaisosajoseph@outlook.com>
2023-07-11 19:50:24 -0700

    Task 1: complete task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates a string to a new memory location
+ * @str: string argument
+ *
+ * Return: returns a pointer to new memory space or NULL if str = NULL
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *dup_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		;
+
+	dup_str = malloc(sizeof(str) * (i + 1));
+
+	if (dup_str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (1)
+	{
+		*(dup_str + i) = *(str + i);
+		if (*(str + i) == '\0')
+			break;
+		i++;
+	}
+
+	return (dup_str);
+}
