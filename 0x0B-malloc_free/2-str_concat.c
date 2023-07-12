86d40f5b1e93fe07ad6f531d7f05143c70ef0362
evil-ghost <danielaisosajoseph@outlook.com>
2023-07-11 20:54:40 -0700

    Task 2: complete task

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string argument
+ * @s2: second string argument
+ *
+ * Return: returns a pointer to new mem location or NULL on fail
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *new_str;
+
+	if (s1 == NULL)
+		s1 = "";
+	else if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; *(s1 + i) != '\0'; i++)
+		;
+	for (j = 0; *(s2 + j) != '\0'; i++, j++)
+		;
+
+	new_str = malloc(sizeof(char) * (i + 1));
+
+	if (new_str == NULL)
+		return (NULL);
+
+	i = 0;
+	while (*(s1 + i) != '\0')
+	{
+		*(new_str + i) = *(s1 + i);
+		i++;
+	}
+
+	j = 0;
+	while (*(s2 + j) != '\0')
+	{
+		*(new_str + i) = *(s2 + j);
+		i++;
+		j++;
+	}
+
+	return (new_str);
+}
