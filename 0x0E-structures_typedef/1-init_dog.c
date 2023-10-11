2992fe0acc19ad41cccda0bd0ec4e3925042f891
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-11 14:16:02 -0700

    Task 1: complete task

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+
+/**
+ * init_dog - a function that initialize a variable of type struct dog
+ * @d: structure argument
+ * @name: char argument
+ * @age: float argument
+ * @owner: char argument
+ *
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
