31eaba556f1ed55a94247369e12edea9de0d61c9
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-17 22:19:15 -0700

    Create function calc file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "calc.h"
+
+/**
+ * op_add - a function to add two integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns the result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - a function to subtract two integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns the result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - a function to multiply two integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns the result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - a function to divide two integers
+ * @a: first argument
+ * @b: second argument
+ *
+ * Return: returns the result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* op_mod - a function to get the remainder of two integers
+* @a: first argument
+* @b: second argument
+*
+* Return: returns the result
+*/
+int op_mod(int a, int b)
+{
+       return (a % b);
+}
