c62151d7e6967ad6403447626595e2f8c43fed0c
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-17 19:11:39 -0700

    Task 0: complete task

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - a function that prints a name
+ * @name: name to be printed
+ * @f: function pointer
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+
+	f(name);
+}
