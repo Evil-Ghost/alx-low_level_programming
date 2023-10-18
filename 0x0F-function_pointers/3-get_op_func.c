b713c1d4bfb5b8314652df8e7eafe6d01424459b
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-17 22:40:50 -0700

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - a function that selects the correct func to perform the oper
+ * @char: operator character
+ *
+ * Return: returns a pointer to function that takes two int arg
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (ops[i].op == *(s))
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
