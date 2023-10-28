3bbb8016baf7da242114538c747721516aeed7ee
evil-ghost <danielaisosajoseph@outlook.com>
2023-10-27 21:22:05 -0700

    Task 5: complete task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint2 - a function that frees a list
+ * @head: beginning of the list
+ *
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *current_node;
+
+	if (*head == NULL)
+		return;
+
+	current_node = *head;
+	while (current_node != NULL)
+	{
+		*head = (*head)->next;
+		free(current_node);
+		current_node = *head;
+	}
+
+}
